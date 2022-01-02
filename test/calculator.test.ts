9dfb7c723d6ddaa05f0351dc945fe942903e542d
Alejandro Ferrera <alejandro.ferrera@outlook.com>
2022-01-01 21:38:22 -0600

    Replace weird double quotes with normal ones

--- test/calculator.test.ts
+++ test/calculator.test.ts
@@ -14,7 +14,7 @@ describe('String Calculator Kata', () => {
 
 // 5. Given the user input is multiple numbers with new line and comma delimiters when calculating the sum then it should return the sum of all the numbers. (example "1\n2,3" should equal 6).
 
-// 6. Given the user input is multiple numbers with a custom single-character delimiter when calculating the sum then it should return the sum of all the numbers. (example “//;\n1;2” should return 3).
+// 6. Given the user input is multiple numbers with a custom single-character delimiter when calculating the sum then it should return the sum of all the numbers. (example "//;\n1;2" should return 3).
 
 // 7. Given the user input contains one negative number when calculating the sum then it should throw an exception "negatives not allowed: x" (where x is the negative number).
 
@@ -22,6 +22,6 @@ describe('String Calculator Kata', () => {
 
 // 9. Given the user input contains numbers larger than 1000 when calculating the sum it should only sum the numbers less than 1001. (example 2 + 1001 = 2)
 
-// 10. Given the user input is multiple numbers with a custom multi-character delimiter when calculating the sum then it should return the sum of all the numbers. (example: “//[***]\n1***2***3” should return 6).
+// 10. Given the user input is multiple numbers with a custom multi-character delimiter when calculating the sum then it should return the sum of all the numbers. (example: "//[***]\n1***2***3" should return 6).
 
-// 11. Given the user input is multiple numbers with multiple custom delimiters when calculating the sum then it should return the sum of all the numbers. (example “//[*][%]\n1*2%3” should return 6).
+// 11. Given the user input is multiple numbers with multiple custom delimiters when calculating the sum then it should return the sum of all the numbers. (example "//[*][%]\n1*2%3" should return 6).
